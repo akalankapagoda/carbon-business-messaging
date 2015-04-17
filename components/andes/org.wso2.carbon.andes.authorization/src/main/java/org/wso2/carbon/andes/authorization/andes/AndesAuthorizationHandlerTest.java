53ecf21edc614a4cd31f8d0579bce2734223641f
Akalanka <akalankapagoda@gmail.com>
2015-04-17 11:42:00 +0530

    Adding mqtt tenant security

---
+++ components/andes/org.wso2.carbon.andes.authorization/src/main/java/org/wso2/carbon/andes/authorization/andes/AndesAuthorizationHandlerTest.java
@@ -0,0 +1,11 @@
+package org.wso2.carbon.andes.authorization.andes;
+
+import junit.framework.TestCase;
+
+public class AndesAuthorizationHandlerTest extends TestCase {
+
+    public void testIsOwnDomain() {
+
+    }
+
+}
